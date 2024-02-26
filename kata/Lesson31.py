67e47671858ad69463b868863ffd638fff9731e8
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-26 23:35:32 +0000

    Add three more lambda functions, one in each case, for the special greetings

--- kata/Lesson31.py
+++ kata/Lesson31.py
@@ -5,27 +5,30 @@ class Lesson31(Song):
     def sing_song(self, style,names):
        if style == 1 :
             name_check = lambda name1: name1.startswith("L")
+            special_greeting = lambda name2: "Hip Hip Horray! For " + name2
 
             for name in names:
                 if (True and name_check(name)):
-                    self.sing("Hip Hip Horray! For " + name)
+                    self.sing(special_greeting(name))
                 else:
                     self.sing("Hello " + name + ", it's nice to meet you.")
 
        elif style == 2:
            name_check = lambda name1: 'a' in name1
+           special_greeting = lambda name2: name2.upper() + "! Yay " + name2 + "!"
 
            for name in names:
                if (True and name_check(name)):
-                   self.sing(name.upper() + "! Yay " + name + "!")
+                   self.sing(special_greeting(name))
                else:
                    self.sing("Hello " + name + ", it's nice to meet you.")
 
        elif style == 3:
            name_check = lambda name1: name1.startswith("L")
+           special_greeting = lambda name2: "Hip Hip Horray! For " + name2
 
            for name in names:
                if (False and name_check(name)):
-                   self.sing("Hip Hip Horray! For " + name)
+                   self.sing(special_greeting(name))
                else:
                    self.sing("Hello " + name + ", it's nice to meet you.")
