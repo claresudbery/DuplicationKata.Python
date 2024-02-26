fc358ac0b883df2847e01ddbbb7998c30cad63a3
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-26 23:38:49 +0000

    Remove Trues from name_check functions

--- kata/Lesson31.py
+++ kata/Lesson31.py
@@ -4,7 +4,7 @@ from kata.Song import Song
 class Lesson31(Song):
     def sing_song(self, style,names):
        if style == 1 :
-            name_check = lambda name1: True and name1.startswith("L")
+            name_check = lambda name1: name1.startswith("L")
             special_greeting = lambda name2: "Hip Hip Horray! For " + name2
 
             for name in names:
@@ -14,7 +14,7 @@ class Lesson31(Song):
                     self.sing("Hello " + name + ", it's nice to meet you.")
 
        elif style == 2:
-           name_check = lambda name1: True and 'a' in name1
+           name_check = lambda name1: 'a' in name1
            special_greeting = lambda name2: name2.upper() + "! Yay " + name2 + "!"
 
            for name in names:
