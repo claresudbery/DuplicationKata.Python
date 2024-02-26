178875a0eb39d7b35bc8ff43e0330e02d0fb9702
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-02-26 23:32:39 +0000

    Move lambda function declarations outside for loops

--- kata/Lesson31.py
+++ kata/Lesson31.py
@@ -4,27 +4,27 @@ from kata.Song import Song
 class Lesson31(Song):
     def sing_song(self, style,names):
        if style == 1 :
+            name_check = lambda name1: name1.startswith("L")
 
             for name in names:
-                name_check = lambda name1: name1.startswith("L")
                 if (True and name_check(name)):
                     self.sing("Hip Hip Horray! For " + name)
                 else:
                     self.sing("Hello " + name + ", it's nice to meet you.")
 
        elif style == 2:
+           name_check = lambda name1: 'a' in name1
 
            for name in names:
-               name_check = lambda name1: 'a' in name1
                if (True and name_check(name)):
                    self.sing(name.upper() + "! Yay " + name + "!")
                else:
                    self.sing("Hello " + name + ", it's nice to meet you.")
 
        elif style == 3:
+           name_check = lambda name1: name1.startswith("L")
 
            for name in names:
-               name_check = lambda name1: name1.startswith("L")
                if (False and name_check(name)):
                    self.sing("Hip Hip Horray! For " + name)
                else:
