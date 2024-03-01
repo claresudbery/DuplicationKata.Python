2c04bda6d610b374bf9539c278bb1d9526e71734
Clare Sudbery <clareworking@claresudbery.co.uk>
2024-03-01 16:17:13 +0000

    Better formatting

--- kata/Lesson31.py
+++ kata/Lesson31.py
@@ -3,16 +3,28 @@ from kata.Song import Song
 
 class Lesson31(Song):
     def sing_song(self, style, names):
-       if style == 1:
-           self.sing_cheers(lambda name1: name1.startswith("L"), names, lambda name2: "Hip Hip Horray! For " + name2)
+        if style == 1:
+            self.sing_cheers(
+                names,
+                lambda name1: name1.startswith("L"),
+                lambda name2: "Hip Hip Horray! For " + name2
+            )
 
-       elif style == 2:
-           self.sing_cheers(lambda name1: 'a' in name1, names, lambda name2: name2.upper() + "! Yay " + name2 + "!")
+        elif style == 2:
+            self.sing_cheers(
+                names,
+                lambda name1: 'a' in name1,
+                lambda name2: name2.upper() + "! Yay " + name2 + "!"
+            )
 
-       elif style == 3:
-           self.sing_cheers(lambda name1: False, names, lambda name2: "Hip Hip Horray! For " + name2)
+        elif style == 3:
+            self.sing_cheers(
+                names,
+                lambda name1: False,
+                lambda name2: "Hip Hip Horray! For " + name2
+            )
 
-    def sing_cheers(self, name_check, names, special_greeting):
+    def sing_cheers(self, names, name_check, special_greeting):
         for name in names:
             if (name_check(name)):
                 self.sing(special_greeting(name))
